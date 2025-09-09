87c0db92a4c0606c43953e961fcaee23c1d27a28
CamiloBueno-dev <cabuenorey@gmail.com>
2025-09-09 14:17:53 -0500

    controlador basico

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', function () {
-    return "list products";
-});
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/create', [ProductController::class, 'create']);
+Route::get('/products/{id}/{category?}', [ProductController::class, 'details']);
 
-Route::get('/products/{id}/{category?}', function ($id, $category = null) {
-    if($category!=null){
-        return "Detail products: " . $id . ". With category: " . $category;
-    }
-    else{
-        return "Detail products: " . $id;
-    }
-});
