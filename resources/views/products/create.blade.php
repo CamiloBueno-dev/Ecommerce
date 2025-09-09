129c5ae21c1da4dc79181f737829dd934fda4b59
CamiloBueno-dev <cabuenorey@gmail.com>
2025-09-09 15:46:29 -0500

    layouts en laravel

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Form - Create Product</h1>
+    <form action="" method="POST">
+
+    </form>
+
+@endsection
--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ecommerce</title>
-</head>
-<body>
-    <h1>Product details: {{ $id }}</h1>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+
+<h1>Product details: {{ $id }}</h1>
+<h1>Product Category: {{ $category }}</h1>
+
+@endsection
