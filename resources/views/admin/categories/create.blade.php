574f9f801bf7b17c01058634d0dbe1bfd548aae5
CamiloBueno-dev <cabuenorey@gmail.com>
2025-10-28 14:47:12 -0500

    admin layouts

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <div class="contenedor">
+        <h1>Crear nueva categoría</h1>
+
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+use App\Models\Category;
+use App\Models\Product;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class,'index']);
 
 Route::prefix("products")->controller(ProductController::class)->group(function () {
     Route::get('/', 'index');
@@ -17,4 +19,8 @@ Route::prefix("products")->controller(ProductController::class)->group(function 
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/', [HomeController::class, 'welcome']);
+
+Route::prefix('admin')->controller(AdminController::class)->group(function(){
+    Route::get('/',[AdminController::class, 'index'])->name('admin.index');
+    Route::get('/categories', [CategoryController::class, 'create'])->name('admin.categories.create');
+});
