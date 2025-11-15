d29fbca02f5aa64ff77ae15bc6f3a6f8e1006643
CamiloBueno-dev <cabuenorey@gmail.com>
2025-11-15 00:06:07 -0500

    validaciones del formulario de products

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -9,39 +9,55 @@
                 @csrf
 
                 <!--Nombre del producto-->
+                @error('name')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
                 <div class="input-group input-group-outline mb-3">
                     <label for="nombre" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="productName" name="name">
+                    <input type="text" class="form-control" id="productName" name="name" value="{{old('name')}}">
                 </div>
 
                 <!--Descripción del producto-->
+                @error('description')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
                 <div class="input-group input-group-outline mb-3">
                     <label for="descripcion" class="form-label">Description</label>
-                    <textarea class="form-control" id="productDescription" rows="4" name="description"></textarea>
+                    <textarea class="form-control" id="productDescription" rows="4" name="description">value="{{old('description')}}"</textarea>
                 </div>
 
                 <!--Precio del producto-->
+                @error('price')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
                 <div class="input-group input-group-outline mb-3">
                     <label for="precio" class="form-label">Price</label>
-                    <input type="number" class="form-control" id="productPrice" name="price">
+                    <input type="number" class="form-control" id="productPrice" name="price" value="{{old('price')}}">
                 </div>
 
                 <!--Categoria del producto-->
+                @error('category')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
                 <div class="input-group input-group-outline mb-3">
                     <select class="form-control" id="productCategory" name="category">
                         <option value="">-- Category --</option>
                         @foreach ($categories as $item)
-                            <option value="{{$item->id}}">{{$item->name}}</option>
+                            <option value="{{ $item->id }}">{{ $item->name }}</option>
                         @endforeach
                     </select>
                 </div>
 
                 <!--Marca del producto-->
+                <!--Categoria del producto-->
+                @error('brand')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
                 <div class="input-group input-group-outline mb-3">
                     <select class="form-control" id="productBrand" name="brand">
                         <option value="">-- Brand --</option>
                         @foreach ($brands as $item)
-                            <option value="{{$item->id}}">{{$item->name}}</option>
+                            <option value="{{ $item->id }}">{{ $item->name }}</option>
                         @endforeach
                     </select>
                 </div>
