97d2032ea981d7f77eef2151ddb41bc306c5e67f
CamiloBueno-dev <cabuenorey@gmail.com>
2025-11-15 15:46:32 -0500

    creacion table brands

--- resources/views/admin/layouts/aside.blade.php
+++ resources/views/admin/layouts/aside.blade.php
@@ -5,7 +5,7 @@
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand px-4 py-3 m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
             target="_blank">
-            <img style="max-height: fit-content!important;" src="{{asset('/assets/img/logos/LogoUNAB/unab_logo.png')}}"
+            <img style="max-height: fit-content!important;" src="{{ asset('/assets/img/logos/LogoUNAB/unab_logo.png') }}"
                 alt="Ecommerce UNAB" class="img-fluid border-radius-lg shadow-sm">
 
         </a>
@@ -22,7 +22,7 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link {{ Request::is('admin/products') ? 'active bg-gradient-dark text-white' : 'text-dark' }}"
-                href="{{route('admin.products.table')}}">
+                    href="{{ route('admin.products.table') }}">
                     <i class="material-symbols-rounded opacity-5">table_view</i>
                     <span class="nav-link-text ms-1">Products</span>
                 </a>
@@ -35,7 +35,8 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark" href="../pages/virtual-reality.html">
+                <a class="nav-link {{ Request::is('admin/brands') ? 'active bg-gradient-dark text-white' : 'text-dark' }}"
+                    href="{{route('admin.brands.table')}}">
                     <i class="material-symbols-rounded opacity-5">view_in_ar</i>
                     <span class="nav-link-text ms-1">Brands</span>
                 </a>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;        
 
 Route::get('/', [ProductController::class,'index']);
@@ -16,6 +17,7 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->controller(AdminController::class)->group(function(){
     Route::get('/',[AdminController::class, 'index'])->name('admin.index');
+
     Route::get('/categories', [CategoryController::class, 'table'])->name('admin.categories.table');
     Route::get('/categories/create', [CategoryController::class, 'create'])->name('admin.categories.create');
     Route::post('/categories/store', [CategoryController::class, 'store'])->name('admin.categories.store');
@@ -25,5 +27,12 @@ Route::prefix('admin')->controller(AdminController::class)->group(function(){
     Route::post('products/store', [ProductController::class, 'store'])->name('admin.products.store');
     Route::get('products', [ProductController::class, 'table'])->name('admin.products.table');
     Route::delete('/products/{product}', [ProductController::class, 'delete'])->name('products.delete');
+
+    Route::get('/brands', [BrandController::class, 'table'])->name('admin.brands.table');
+    Route::get('/brands/create', [BrandController::class, 'create'])->name('admin.brands.create');
+    Route::post('/brands/store', [BrandController::class, 'store'])->name('admin.brands.store');
+    Route::delete('/brands/{brand}', [BrandController::class, 'delete'])->name('admin.brands.delete');
+
+
 
 });
